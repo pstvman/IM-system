f7f465c03bc10bd1a4b3bba120b7403d8af88161
pstv <jaby0919@outlook.com>
2025-03-26 17:37:39 +0800

    v0.2 用户上线提醒功能

--- server.go
+++ server.go
@@ -3,12 +3,20 @@ package main
 import (
   "fmt"
   "net"
+  "sync"
 )
 
 
 type Server struct {
   Ip    string
   Port  int
+
+  //在线用户列表
+  OnlineMap map[string]*User
+  mapLock sync.RWMutex
+
+  // 消息广播的channel
+  Message chan string
 }
 
 // 创建一个Server的接口
@@ -16,6 +24,8 @@ func NewServer(ip string, port int) *Server {
   server := &Server{
     Ip: ip,
     Port: port,
+    OnlineMap: make(map[string]*User),
+    Message: make(chan string),
   }
 
   return server
@@ -32,6 +42,9 @@ func (this *Server) Start() {
   // close listen socket
   defer listener.Close()
 
+  // 启动监听Message的goroutine
+  go this.ListenMessager()
+
   for {
     // accept
     conn, err := listener.Accept()
@@ -47,5 +60,38 @@ func (this *Server) Start() {
 
 func (this *Server) Handler(conn net.Conn) {
   // 当前链接的业务
-  fmt.Println("链接建立成功")
+  // fmt.Println("链接建立成功")
+
+  user := NewUser(conn)
+
+  // 用户上线，将用户加入到onlineMap中
+  this.mapLock.Lock()
+  this.OnlineMap[user.Name] = user
+  this.mapLock.Unlock()
+
+  // 广播当前用户上线消息
+  this.BroadCast(user, "已上线")
+
+  // 当前handler阻塞
+  select {}
+}
+
+// 广播消息的方法
+func (this *Server) BroadCast(user *User, msg string) {
+  sendMsg := "[]" + user.Addr + "]" + user.Name + ":" + msg
+  this.Message <- sendMsg
+}
+
+// 监听Message广播消息channel的goroutine，一旦有消息就发送给全部在线User
+func (this *Server) ListenMessager() {
+  for {
+    msg := <- this.Message
+
+    // 将msg发送给全部在线User
+    this.mapLock.Lock()
+    for _, cli := range this.OnlineMap {
+      cli.C <- msg
+    }
+    this.mapLock.Unlock()
+  }
 }
