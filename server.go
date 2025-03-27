d4975baa9831d64385d4a5a76f52e80e5abf219e
pstv <jaby0919@outlook.com>
2025-03-27 23:59:03 +0800

    v0.3 用户消息广播机制

--- server.go
+++ server.go
@@ -4,6 +4,7 @@ import (
   "fmt"
   "net"
   "sync"
+  "io"
 )
 
 
@@ -71,6 +72,28 @@ func (this *Server) Handler(conn net.Conn) {
 
   // 广播当前用户上线消息
   this.BroadCast(user, "已上线")
+
+  // 接收客户端发送的消息
+  go func() {
+    buf := make([]byte, 4096)
+    for {
+      n, err := conn.Read(buf)
+      if n == 0 {
+        this.BoradCast(user, "下线")
+        return
+      }
+
+      if err != nil && err != io.EOF {
+        fmt.Println("Conn Read err: ", err)
+        return
+      }
+
+      // 提取用户的消息，去除"\n"
+      msg := string(buf[:n-1])
+      // 将得到的消息进行广播
+      this.BroadCast(user, msg)
+    }
+  }()
 
   // 当前handler阻塞
   select {}
