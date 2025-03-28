2103950184af4825f430a0f777ae310997668c3d
pstv <jaby0919@outlook.com>
2025-03-28 10:33:12 +0800

    v0.4 用户业务封装

--- user.go
+++ user.go
@@ -8,16 +8,20 @@ type User struct {
 	Addr string
 	C chan string
 	conn net.Conn
+
+	server *Server
 }
 
 // 创建一个User对象的接口
-func NewUser(conn net.Conn) *User {
+func NewUser(conn net.Conn, server *Server) *User {
 	userAddr := conn.RemoteAddr().String()
 	user := &User{
 		Name: userAddr,
 		Addr: userAddr,
 		C: make(chan string),
 		conn: conn,
+
+		server: server,
 	}
 
 	// 启动监听当前user channel消息的goroutine
@@ -32,4 +36,31 @@ func (this *User) ListenMessage() {
 		msg := <- this.C
 		this.conn.Write([]byte(msg + "\n"))
 	}
+}
+
+// 用户上线
+func (this *User)Online() {
+	//将用户加入到onlineMap中
+	this.server.mapLock.Lock()
+  this.server.OnlineMap[this.Name] = this
+  this.server.mapLock.Unlock()
+
+	// 广播当前用户上线消息
+  this.server.BroadCast(this, "已上线")
+}
+
+// 用户下线
+func (this *User)Offline() {
+	this.server.mapLock.Lock()
+  delete(this.server.OnlineMap, this.Name)
+  this.server.mapLock.Unlock()
+
+	// 广播当前用户下线消息
+	this.server.BroadCast(this, "下线")
+}
+
+// 消息处理
+func (this *User)DoMessage(msg string) {
+		// 发送消息
+		this.server.BroadCast(this, msg)
 }
